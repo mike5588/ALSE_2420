d2ab45d6db7ca228c5601078cb7f795af09c0a45
alxperezECI <47115365+alxperezECI@users.noreply.github.com>
2024-09-02 15:01:26 -0500

    Subiendo el primer ejemplo
    Este ejemplo crea dos archivos ejecutables que solicitan dos números al usuario y luego los imprime en orden inverso. Un ejecutable lo hace en C y el otro en C++.

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+  int x, y ,aux;
+  printf("Ingrese dos números enteros: ") ;
+  scanf("%i", &x);
+  scanf("%i", &y);
+  aux = x;
+  x = y;
+  y = aux;
+  printf("%i, %i\n", x, y);
+  return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+
+  int x, y ,aux;
+  std::cout << "Ingrese dos números enteros: " ;
+  std::cin >> x >> y;
+  aux = x;
+  x = y;
+  y = aux;
+  std::cout << x << ", " << y << std::endl;
+  std::cout << "Hola Alse" << std::endl;
+  return 0;
+}
