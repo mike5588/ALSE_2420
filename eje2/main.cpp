6050e266e512f493ee0dc72089d861a756f91781
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2024-09-03 17:40:45 -0500

    Ejercicio para la clase.

---
+++ eje2/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(){
+  // Defina un vector de flotantes de 50 elementos e 
+  // inicialicelo todo en 0.0 usando un for
+  
+
+
+  // Usando la función de números aleatorios llene el vector con 
+  // con datos entre 90 y 250 dejando en 0 el valor de la posición 25
+
+
+  // Muestre el contenido del vector en la pantalla en un arreglo de
+  // 10 filas por 5 columnas
+
+
+  // Ordene el vector de menor a mayor con el método de la burbuja
+
+
+
+  // Muestre nuevamente el contenido del vector en la pantalla en un 
+  // arreglo de 10 filas por 5 columnas
+
+  return 0;
+}
