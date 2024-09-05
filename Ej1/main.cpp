c13f3391c5032ee95a79774d71a8a77b7292f2f4
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2024-09-05 11:05:11 -0500

    Cambio en el main.cpp para incluir comentario acerca del trabajo desde el VSCode en Windows.

--- Ej1/main.cpp
+++ Ej1/main.cpp
@@ -10,5 +10,6 @@ int main(){
   y = aux;
   std::cout << x << ", " << y << std::endl;
   std::cout << "Hola Alse" << std::endl;
+  std::cout << "Nuestro primer contacto con el VSCode de Windows con un proyecto en WSL2\n";
   return 0;
 }
