d65f394cc8212dd49236aa6dc7ca2b3f469179fc
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2024-09-13 14:35:40 -0500

    switch

--- eje2/main.cpp
+++ eje2/main.cpp
@@ -34,5 +34,36 @@ int main(){
   // Muestre nuevamente el contenido del vector en la pantalla en un 
   // arreglo de 10 filas por 5 columnas
 
+  int x;
+  std::cout << "Ingrese un número de 1 a 5\n";
+  std::cin >> x;
+  switch ( x )
+  {
+  case 1:
+    /* code */
+    std:: cout << "Ingresó uno\n";
+    break;
+  case 2:
+    /* code */
+    std:: cout << "Ingresó dos\n";
+    break;
+  case 3:
+    /* code */
+    std:: cout << "Ingresó tres\n";
+    break;
+  case 4:
+    /* code */
+    std:: cout << "Ingresó cuatro\n";
+    break;
+  case 5:
+    /* code */
+    std:: cout << "Ingresó cinco\n";
+    break;
+  default:
+    std::cout << "No siguió las indicaciones\n";
+    return 1;
+  }
+
+  std::cout << "Gracias por seguir las indicaciones\n";
   return 0;
 }
--- eje3/main.cpp
+++ eje3/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
 #include <stdlib.h>
-#include <time.h>
+#include <cmath>
+
+using namespace std;
+
+int mysuma( const float &a, int b)
+{
+  //a = 777.9;
+  return (int)(a + (float)b);
+}
 
 int main(){
   
-  
+  int a, b, c;
+  float d, e, f;
+  double h, i, j;
+
+  a = 7;
+  b = 9;
+  d = 3.141592;
+  e = 78.87;
+  h =  667.897;
+  i = (double) e;
+  // int max( int a , int b )
+  c = max(a, b);
+  // float max( float a , float b )
+  f = max( d, e );
+  // double max( double a, double b )
+  j = max( h, i );
+  cout << "Los máximos son: " << c << " que usa " << sizeof(c) << endl;
+  cout << f << " que usa " << sizeof(f) << " y " << j << " que usa "<< sizeof(j) << endl;
+  cout << "La suma de un flotante y un entero: " << mysuma(f,c) << endl;
+  cout << f << ", " << &f << endl;
   return 0;
 }
