d1a25e12271dea0c6c6c47f91f387a0a5139c2cc
Alexander Pérez Ruiz <alexander.perez@escuelaing.edu.co>
2024-10-01 11:11:14 -0500

    Cambiando de usar una struct a una class.

--- clase_complejo/complex.cpp
+++ clase_complejo/complex.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "complex.h"
+#include <cmath>
 
 using namespace std;
 
@@ -26,58 +27,73 @@ double Complex::Im()
 
 Complex::Complex(double r =0. , double i = 0 )
 {
-
+  re = r;
+  im = i;
 }
 
-double Complex::angle(){
-
+double Complex::angle()
+{
+  return atan2(im, re);
 }
 
 double Complex::magnitude()
 {
-
+  return sqrt(re*re + im*im);
 }
 
 Complex Complex::conjugate()
 {
-
+  Complex r;
+  r.re = re;
+  r.im = -im;
+  return r;
 }
 
 Complex Complex::operator+( const Complex &a )
 {
+  Complex res;
 
+  return res;
 }
 
 Complex Complex::operator-( const Complex &a )
 {
+  Complex res;
 
+  return res;
 }
 
 Complex Complex::operator*( const Complex &a )
 {
+  Complex res;
 
+  return res;
 }
 
-bool Complex::operator==( const Complex &a );
+bool Complex::operator==( const Complex &a )
 {
+  bool res;
 
+  return res;
 }
 
-bool Comnplex::operator<( const Complex &a );
+bool Comnplex::operator<( const Complex &a )
 {
+  bool res;
 
+  return res;
 }
 
-bool Complex::operator>( const Complex &a );
+bool Complex::operator>( const Complex &a )
 {
+  bool res;
 
+  return res;
 }
 
 
-ostream& operator<<(ostream& out, Complex a){
-
-}
-
+ostream& operator<<(ostream& out, Complex a)
 {
-
+  out << a.re << " + " << a.im << "j";
+  return out;
 }
--- clase_complejo/main.cpp
+++ clase_complejo/main.cpp
@@ -1,46 +1,34 @@
 #include <iostream>
 #include "complex.h"
 
-/*  TALLER: 
-    - Complete el CMakeLists.txt para configurar adecuadamente el proyecto. 
-    - Nombrar el ejecutable usando la variable de configuración PROJECT_NAME
-    - Usar tanto el main.cpp como el complex.h para construir el ejecutable.
-    - Completar el archivo complex.h de acuerdo a las indicaciones internas.
-	- No se debe modificar el código del main.cpp y la aplicación debe 
-	    ejecutarse correctamente. Se deben explicar cada una de las líneas 
-		del main en comentarios.
-	- Enviar el pull request.
-	- Se puede desarrollar de forma individual o máximo en parejas.
-*/
-
 	
 using namespace std;
 
 int main(){
-  complex a, b, c;
-  a.re = 34.8; a.im = -12.4;
-  b = conjugate(a);
-  c.re = c.im = 24.59;
-
-  complex d =  c + b;
-
-  //d = c - a;
+  Complex a(34.8, -12.4), b, c(24.59, 24.59);
+  //a.re = 34.8; a.im = -12.4;
+  //b = conjugate(a);
+  b = a.conjugate();
+  //c.re = c.im = 24.59;
   cout << c << endl;
 
+  Complex d =  c + b;
+  cout << d << endl;
+  d = c - a;
   cout << d << endl;
 
   cout << "Son iguales a y b?: " << ( a==b ) << endl;
   cout << "son iguales b y d?: " << ( b==d ) << endl;
 
 // Ahora con arrglos
-  complex obj[5];
-  obj[0].re = 34.; obj[0].im = -4.;
-  obj[1].re = obj[1].im = 45.;
-  obj[2].re 33.33; obj[2].im = -45.;
-  obj[3].re = 12.; obj[3].im = 25.;
-  obj[4].re = obj[4].im = -56.56 );
+  Complex obj[5];
+  obj[0].Re( 34. ); obj[0].Im( -4. );
+  obj[1].Re( 45. ); obj[1].Im( 45. );
+  obj[2].Re( 33.33 ); obj[2].Im( -45. );
+  obj[3].Re( 12. ); obj[3].Im( 25. );
+  obj[4].Re(-56.56 ); obj[4].Im( -56.56 );
 
-  double tmp1, tmp2;
+  Complex tmp1;
   bool cambio;
 
   // Qué hace este código?
@@ -49,12 +37,10 @@ int main(){
     for (int i =0; i < 4; ++i ){
       if( obj[i] < obj[i+1] ){
       }else{
-        tmp1 = obj[i].re;
-        tmp2 = obj[i].im;
-        obj[i].re = obj[i+1].re ;
-        obj[i].im = obj[i+1].im ;
-        obj[i+1].re = tmp1 ;
-        obj[i+1].im = tmp2 ;
+        tmp1 = obj[i];
+        obj[i].Re( obj[i+1].Re() );
+        obj[i].Im( obj[i+1].Im() );
+        obj[i+1] = tmp1 ;
         cambio = true;
       }
     }
