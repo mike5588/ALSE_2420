710fd6ce87eba0e80a477b0d7989d9799603346f
mike5588 <miguel.pardo-r@mail.escuelaing.edu.co>
2024-10-31 11:27:18 -0500

    Miguel pardo, juan ospina

--- p2_pra/main.cpp
+++ p2_pra/main.cpp
@@ -5,43 +5,49 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <map>
 
 #include "empleado.h"
 
 using namespace std;
 
-void cargarArchivo( char* file, vector<Empleado> &trabajadores ){
-  string nombre, apellido;
-  unsigned int cedula;
-  double salario;
-  int dia;
-  float horas;
-  string   texto;
-  stringstream ss;
-  
-  ifstream archivo( file );
-  if( archivo.is_open() ){
-    while( !archivo.eof() && getline( archivo, texto)){
-      ss.clear();
-      ss << texto ; 
-      ss >> nombre >> apellido >> cedula >> salario;
-      Empleado e( nombre, apellido, cedula, salario );
-      while( ss >> dia >> horas ){
-        e.HorasExtras( dia, horas );
-      }
-      trabajadores.push_back( e );
+void cargarArchivo(char* file, vector<Empleado>& trabajadores) {
+    string nombre, apellido;
+    unsigned int cedula;
+    double salario;
+    int dia;
+    float horas;
+    string texto;
+    stringstream ss;
+
+    ifstream archivo(file);
+    if (archivo.is_open()) {
+        while (!archivo.eof() && getline(archivo, texto)) {
+            ss.clear();
+            ss << texto;
+            ss >> nombre >> apellido >> cedula >> salario;
+            Empleado e(nombre, apellido, cedula, salario);
+            while (ss >> dia >> horas) {
+                e.agregarHorasExtras(dia, horas);
+            }
+            trabajadores.push_back(e);
+        }
+        archivo.close();
     }
-    archivo.close();
-  }
 }
 
-int main(int argc, char**argv){
-  float c = 0.;
-  vector<Empleado> _trabajadores;
-  
-  cargarArchivo( argv[1], _trabajadores );//carga bien
-  size_t tam = _trabajadores.size();
- cout << "Empleado (Nombre y apellido) | Salario mensual | Horas extras | Total a pagar" << endl;
+int main(int argc, char** argv) {
+    vector<Empleado> _trabajadores;
+
+    cargarArchivo(argv[1], _trabajadores); // carga bien
+    size_t tam = _trabajadores.size();
+    cout << "Número de trabajadores: " << tam << endl;
+
+    // Calcula el valor a pagar por horas extras de cada trabajador 
+    // y mostrar el valor mensual y el valor de las horas extras en un 
+    // arreglo tabular
+    // Empleado (Nombre y apellido) | Salario mensual | Horas extras | Total a pagar
+    cout << "Empleado (Nombre y apellido) | Salario mensual | Horas extras | Total a pagar" << endl;
     for (size_t i = 0; i < _trabajadores.size(); ++i) {
         double salarioTotal = _trabajadores[i].calcularSalario();
         cout << _trabajadores[i].getNombre() << " " << _trabajadores[i].getApellido() << " | "
@@ -50,16 +56,20 @@ int main(int argc, char**argv){
              << salarioTotal << endl;
     }
 
-/* Encontrar y reportar el empleado con mayor número de horas extras
-*/
- // Encontrar y reportar el empleado con mayor número de horas extras
+    // Encontrar y reportar el empleado con mayor número de horas extras
+    Empleado* maxHorasExtras = &_trabajadores[0];
+    double maxHoras = _trabajadores[0].calcularSalario() - _trabajadores[0].getSalarioMensual();
+    for (size_t i = 1; i < _trabajadores.size(); ++i) {
+        double horasExtras = _trabajadores[i].calcularSalario() - _trabajadores[i].getSalarioMensual();
+        if (horasExtras > maxHoras) {
+            maxHoras = horasExtras;
+            maxHorasExtras = &_trabajadores[i];
+        }
+    }
 
+    // Encontrar y reportar el día con mayor número de horas extras
+    cout << "Día con mayor número de horas extras: Día " << diaMaxHoras << " con " << maxHorasDia << " horas" << endl;
 
-/* Encontrar y reportar el empleado con menor número de horas extras
-*/
-
-/* Encontrar y reportar el día con mayor número de horas extras
-*/
-
-  return 0;
+    return 0;
 }
+
